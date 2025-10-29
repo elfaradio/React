1b69154c64add22152923486a7b7fcaada49ad0f
elfaradio <farhadulislam84054@gmail.com>
2025-10-29 23:19:15 +0600

    done

---
+++ 04TailwindProops/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Card from "./components/Card";
+function App() {
+  return (
+    <>
+      <h1>Card1</h1>
+      <Card username="rakib" />
+      <h1>Card2</h1>
+      <Card username="ctg"></Card>
+    </>
+  );
+}
+
+export default App;
